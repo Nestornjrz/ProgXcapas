b593b877244745e5f5e3c14647b045fd0fa61828
Nestornjrz <nestornjrz@hotmail.com>
2017-03-23 13:15:02 -0300

    Se agrega el ClientesManagers pero en este punto el visual studio pide que se instale entityFramework en el proyecto PpcLoca.Domain.Managers

--- PpcLocal/PpcLocal.Domain.Managers/ClientesManagers.cs
+++ PpcLocal/PpcLocal.Domain.Managers/ClientesManagers.cs
@@ -1,6 +1,20 @@
-﻿namespace PpcLocal.Domain.Managers
+﻿using PpcLocal.Domain.Db;
+using PpcLocal.Domain.Dto;
+
+namespace PpcLocal.Domain.Managers
 {
     public class ClientesManagers
     {
+        public static async Task<List<ClienteDto>> GetAllList()
+        {
+            using (var context = new CapasDbEntities())
+            {
+                var listado = await context.Clientes.Select(s => new ClienteDto()
+                {
+
+                }).ToListAsync();
+                return listado;
+            }
+        }
     }
 }
