bfd0f1f3cb3e885f116117277ea04fca72ec8735
Nestornjrz <nestornjrz@hotmail.com>
2017-03-27 16:52:35 -0400

    Se agrega un Unit Test

---
+++ PpcAzure/PpcAzure.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PpcAzure.Domain.Dto;
+using PpcAzure.Domain.Managers;
+using System.Collections.Generic;
+
+namespace PpcAzure.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var tarea = ClientesManagers.GetAllList();
+            tarea.Wait();
+
+            List<ClienteDto> listado = tarea.Result;
+            Assert.IsTrue(listado.Count > 0);
+        }
+    }
+}
