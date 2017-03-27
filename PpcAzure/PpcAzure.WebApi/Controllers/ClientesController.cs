ba154533665a4219d902b27a4316c1448f9de720
Nestornjrz <nestornjrz@hotmail.com>
2017-03-27 15:57:57 -0400

    Instalacion de Swagger

---
+++ PpcAzure/PpcAzure.WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,23 @@
+﻿using PpcAzure.Domain.Dto;
+using PpcAzure.Domain.Managers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace PpcAzure.WebApi.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        [ResponseType(typeof(IEnumerable<ClienteDto>))]
+        public async Task<IHttpActionResult> Get()
+        {
+            var listado = await ClientesManagers.GetAllList();
+            if (listado == null)
+            {
+                return NotFound();
+            }
+            return Ok(listado);
+        }
+    }
+}
